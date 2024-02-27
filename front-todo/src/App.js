d9778daa8d963d8b27c21f9226c06fe49f3aca29
madhu-c10 <pmadhu.c10@gmail.com>
2024-02-27 21:07:36 +0530

    useeffect

--- front-todo/src/App.js
+++ front-todo/src/App.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+
+  const[countone,setCountOne] = useState(0);
+  const[colculation,setColculation] = useState(0);
+
+  const [count,setCount]=useState(0);
+
+  useEffect(()=>{
+    setTimeout(() => {
+      setCount((count)=>count+1)
+    },1000);
+  },[])
+
+  const [value,setValue] = useState({
+    name:"madhu",
+    age:21,
+    skills:"javascript"
+  }) 
+
+  const changeLanguage =()=>{
+    setValue(privios=>{
+      return {...privios,skills:"react"}
+    })
+  }
+
+  useEffect(()=>{
+    setColculation(()=>countone*2)
+  },[countone])
+
   return (
-    <div>App</div>
+  <>
+  <h1>{value.skills}</h1>
+  <button type='button' onClick={changeLanguage}>change</button>
+  <h1>counter{count}</h1>
+
+  <h1>useEffect counter {countone}</h1>
+  <button type='button' onClick={()=> setCountOne((c)=>c+1)}>+</button>
+  <h1>counter two {colculation}</h1>
+  </>
   )
 }
 
